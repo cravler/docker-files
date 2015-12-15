fdab21ab3e891e7cb9d1bed4d9b479cad59319c8
cravler <sergeizz@gmail.com>
2015-12-15 14:08:53 +0200

    [php5-cli] TZ=UTC, PHP5_DATE_TIMEZONE=

--- php5-cli/php5-cli-config.sh
+++ php5-cli/php5-cli-config.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-: ${PHP5_DATE_TIMEZONE:=Europe/London}; export PHP5_DATE_TIMEZONE
+: ${PHP5_DATE_TIMEZONE:=}; export PHP5_DATE_TIMEZONE
 : ${COMPOSER_GITHUB_OAUTH:=}; export COMPOSER_GITHUB_OAUTH
 
 # Apply default or user specified options to config files
+if [[ -z "$PHP5_DATE_TIMEZONE" ]]; then
+    PHP5_DATE_TIMEZONE="$TZ"
+fi
 /.cravler/php5-set-config.sh "$CONF_DIR_PHP5_CLI" 'date.timezone' "$PHP5_DATE_TIMEZONE"
 
 # Composer config
