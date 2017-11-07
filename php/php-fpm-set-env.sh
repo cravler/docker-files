90b7927fc187b555e5e1285ccf3f51c61ece9712
cravler <sergeizz@gmail.com>
2017-11-07 09:34:42 +0200

    [php] improvements

--- php/php-fpm-set-env.sh
+++ php/php-fpm-set-env.sh
@@ -6,13 +6,13 @@ if [ "$_env" ]; then
         for env in `printenv`; do
             IFS== read name value <<< "$env"
             if [ "$value" ]; then
-                echo "env[$name] = $value" >> "$CONF_DIR_PHP_FPM/php-fpm.conf"
+                echo "env[$name] = \$$_env" >> "$CONF_DIR_PHP_FPM/php-fpm.conf"
             fi
         done
     else
         _value=`printenv $_env`
         if [ "$_value" ]; then
-            echo "env[$_env] = $_value" >> "$CONF_DIR_PHP_FPM/php-fpm.conf"
+            echo "env[$_env] = \$$_env" >> "$CONF_DIR_PHP_FPM/php-fpm.conf"
         fi
     fi
 fi
